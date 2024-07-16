3392cdc1fa1635b6a52b524539b8938189d5af90
Eros Secundus <erossecundus@gmail.com>
2024-07-16 09:34:50 -0300

    first commit

---
+++ src/app/components/product/product.component.ts
@@ -0,0 +1,30 @@
+import { Component, EventEmitter, Input, Output } from '@angular/core';
+import { Category } from '../../interfaces/Category';
+import { Product } from '../../interfaces/Product';
+
+@Component({
+  selector: 'app-product',
+  templateUrl: './product.component.html',
+  styleUrl: './product.component.css'
+})
+export class ProductComponent {
+  title = 'Formulário de Cadastro:';
+
+  @Input()
+  categories : Category[] = [];
+
+  @Input()
+  product ?: Product;
+
+  @Output()
+  saveEmitter = new EventEmitter();
+
+  save() {
+    this.saveEmitter.emit();
+  }
+
+  constructor() {}
+
+  ngOnInit() : void {}
+
+}
---
+++ src/app/components/products/products.component.ts
@@ -0,0 +1,49 @@
+import { Component } from '@angular/core';
+import { Category } from '../../interfaces/Category';
+import { Product } from '../../interfaces/Product';
+import { CategoryService } from '../../services/category.service';
+import { ProductService } from '../../services/product.service';
+
+@Component({
+  selector: 'app-products',
+  templateUrl: './products.component.html',
+  styleUrl: './products.component.css'
+})
+export class ProductsComponent {
+  title = 'Produtos Cadastrados:';
+
+  categories: Category[] = [];
+
+  product: Product = {} as Product;
+  products: Product[] = [];
+
+  constructor(private categoryService: CategoryService,
+    private productService: ProductService) { }
+
+  ngOnInit(): void {
+    this.loadCategories();
+    this.loadProducts();
+  }
+
+  loadCategories() {
+    this.categoryService.getCategories().subscribe({
+      next: data => { this.categories = data }
+    });
+  }
+
+  loadProducts() {
+    this.productService.getProducts().subscribe({
+      next: data => { this.products = data }
+    });
+  }
+
+
+  saveProduct() {
+    this.productService.save(this.product).subscribe({
+      next: data => {
+        this.products.push(data);
+        this.product = {} as Product;
+      }
+    });
+  }
+}
